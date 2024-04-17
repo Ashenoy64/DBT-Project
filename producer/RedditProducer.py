92996683f475b83a8b5382d7a4af5427a6f75427
Meowmaster <ashenoy64@gmail.com>
2024-04-17 09:20:54 +0530

    Addede Reddit DataStream

--- producer/RedditProducer.py
+++ producer/RedditProducer.py
@@ -1,7 +1,11 @@
-from confluent_kafka import Producer
+# from confluent_kafka import Producer
+from kafka import KafkaProducer
 import praw
 import os
 from dotenv import load_dotenv
+import time
+import json
+import tqdm
 
 load_dotenv()
 
@@ -11,43 +15,62 @@ KAFAKA_SERVER = os.getenv('KAFKA_CLUSTER_BOOTSTRAP_SERVERS')
 CLIENT_ID = os.getenv('REDDIT_CLIENT_ID')
 CLIENT_SECRET = os.getenv('REDDIT_CLIENT_SECRET')
 USER_AGENT = os.getenv('REDDIT_USER_AGENT')    
+SUBREDDIT = os.getenv('REDDIT_SUBREDDIT')
+INTERVAL = os.getenv('INTERVAL')
 
 
 
+POST_DATA = []
+
+def get_data():
+    global POST_DATA
+    reddit = praw.Reddit(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, user_agent=USER_AGENT)
+    HOT_POSTS = reddit.subreddit(SUBREDDIT).hot()
+    for post in tqdm.tqdm(list(HOT_POSTS),total=len(list(HOT_POSTS)),desc="Getting Data"):
+        _comments =[]
+        for comment in post.comments:
+            comment_data = {
+                "body": comment.body,
+                "score": comment.score,
+                "created_utc": comment.created_utc,
+                "id": comment.id,
+                "permalink": comment.permalink,
+                "ups": comment.ups,
+                "downs": comment.downs,
+                "author": comment.author.name if comment.author else "Deleted",
+
+            }
+            _comments.append(comment_data)
+        post_data = {
+            "title": post.title,
+            "selftext": post.selftext,
+            "url": post.url,
+            "score": post.score,
+            "authorName": post.author.name,
+            "id": post.id,
+            "created_utc": post.created_utc,
+            "comments": post.comments,
+            "permalink": post.permalink,
+            "ups": post.ups,
+            "downs": post.downs,
+            "num_comments": post.num_comments,
+            "comments": _comments,
+        }
+        POST_DATA.append(post_data)
+
+    
+def start_streaming(interval=1):
+    global POST_DATA
+    for i in range(len(POST_DATA)):
+        time.sleep(interval)
+        print("Posting data to Kafka")
+        print(json.dumps(POST_DATA[i],indent=4))
+        break
+
+if __name__ == "__main__":
+    get_data()
+    print("Done Getting Data")
+    start_streaming()
+    pass
 
 
-"""
-reddit = praw.Reddit(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, user_agent=USER_AGENT)
-HOT_POSTS = reddit.subreddit("indian").hot()
-
-for post in HOT_POSTS:
-    print(post.title)
-    print(post.selftext)
-    print(post.url)
-    print(post.score)
-    print(post.id)
-    print(post.subreddit)
-    print(post.created_utc)
-    print(post.author)
-    print(post.comments)
-    print(post.permalink)
-    print(post.upvote_ratio)
-    print(post.ups)
-    print(post.downs)
-    print(post.num_comments)
-    print(post)
-    break
-"""
-
-
-producer = Producer({'bootstrap.servers': "100.88.89.141:9092,100.88.89.141:9093,100.88.89.141:9094"})
-producer.produce('reddit', 'Hello, Kafka!'.encode('utf-8'))
-producer.flush()
-
-
-"""
-producer = KafkaProducer(bootstrap_servers=KAFAKA_SERVER.split(','))
-
-producer.send('reddit', b'Hello, Kafka!')
-producer.flush()
-"""
